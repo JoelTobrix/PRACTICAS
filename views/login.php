10de3fe9a7f1e63a6bbc8e8fa99e60535110f47e
JoelTobrix <pepedariojoel007@gmail.com>
2024-10-17 22:40:36 -0500

    Subir primer avance practico

---
+++ views/login.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <title>Inter.@  v5.0</title>
+        <!-- Bootstrap core CSS -->
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="../css/login.css" rel="stylesheet">
+    </head>
+    <body class="text-center">
+        <main class="form-signin">
+            <form>
+                
+            <img src="../img/icon_ussuary.png" height="50px" weight="50px"> 
+                <h1 class="h3 mb-3 fw-normal">Iniciar sesion</h1>
+                <label for="inputEmail" class="visually-hidden">Email address</label>
+                <input type="email" id="inputEmail" class="form-control" required="" autofocus="" placeholder="Direccion email">
+                <label for="inputPassword" class="visually-hidden">Password</label>
+                <input type="password" id="inputPassword" class="form-control" required="" placeholder="Contraseña">
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me"> Remember me
+                    </label>
+                </div>
+                <button class="w-100 btn btn-lg btn-primary" type="submit">Iniciar sesion</button>
+                
+                <p class="mt-5 mb-3 text-muted">© 2024-2025</p>
+            </form>
+        </main>
+        <script src="/js/popper.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+    </body>
